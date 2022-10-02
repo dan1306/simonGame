ed46e78ecee9122437f7c78c2a4bc54d8f6c3493
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-10-02 19:43:54 -0400

    game functionality

--- game.js
+++ game.js
@@ -15,9 +15,36 @@ $(document).on('keypress', function(e) {
       nextSequence()
       started = true
     }
-  })
+})
+  
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+        if (userClickedPattern.length == gamePattern.length) {
+
+            setTimeout(() => {
+
+                nextSequence()
+              }, 1000)
+              userClickedPattern = []
+        }
+    } else {
+        playSound('wrong')
+        
+        $('body').addClass('game-over')
+
+        setTimeout(() => {
+    
+          $('body').removeClass('game-over')
+        }, 1000)
+    
+        $("h1").text("Game Over, Press Any Key to Restart")
+    }
+    
+}
 
 function nextSequence() {
+
+    level++
 
     $('h1').text(`Level ${level}`)
 
@@ -44,6 +71,7 @@ $('.btn').click(function () {
     console.log(userClickedPattern)
     playSound(userChosenColour)
     animatePress(userChosenColour)
+    checkAnswer(userClickedPattern.length - 1)
 })
 
 function animatePress(clr) {
