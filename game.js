15c928ab7475f458c6779d026cf654b811044103
dan1306 <81488150+dan1306@users.noreply.github.com>
2022-10-02 19:28:38 -0400

    animations

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+let userClickedPattern = []
 
 let buttonColours = ["red", "blue", "green", "yellow"]
 
@@ -19,4 +20,20 @@ function nextSequence() {
 function playSound(name) {
     var audio = new Audio('sounds/' + name + '.mp3');
     audio.play();
+}
+
+$('.btn').click(function () {
+
+    let userChosenColour = this.id
+    userClickedPattern.push(userChosenColour)
+    console.log(userClickedPattern)
+    playSound(userChosenColour)
+    animatePress(userChosenColour)
+})
+
+function animatePress(clr) {
+    $("#" + clr).addClass("pressed");
+    setTimeout(function() {
+      $("#" + clr).removeClass("pressed");
+    }, 100);
 }
